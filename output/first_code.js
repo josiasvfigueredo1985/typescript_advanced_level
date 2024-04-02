ab1ff9272277caa62ebe3b15c4a4e85250c4e679
Josias Valentim <JOSIAS.FIGUEIREDO-ext@ab-inbev.com>
2024-04-02 17:49:24 -0300

    TS Config Adjustments

--- output/first_code.js
+++ output/first_code.js
@@ -1,17 +1,13 @@
+"use strict";
 console.log("Hello world! This is my first TypeScript code!");
-
 const user = "John";
 console.log("Hello " + user + "!. Welcome!");
-    
-class Product{
-    name: string;
-    value: number;
-    constructor(productName: string, productValue: number) {
+class Product {
+    constructor(productName, productValue) {
         this.name = productName;
         this.value = productValue;
     }
 }
-
-const product = new Product('Playstation 5', 5000)
+const product = new Product('Playstation 5', 5000);
 console.log(product);
-
+//# sourceMappingURL=first_code.js.map
