6705f24f5d6aaa1edb9c4d91b54f361e1280adc7
Josias Valentim <JOSIAS.FIGUEIREDO-ext@ab-inbev.com>
2024-04-02 18:06:37 -0300

    Class practices

--- output/first_code.js
+++ output/first_code.js
@@ -1,13 +1,18 @@
 "use strict";
 console.log("Hello world! This is my first TypeScript code!");
-const user = "John";
+var user = "John";
 console.log("Hello " + user + "!. Welcome!");
-class Product {
-    constructor(productName, productValue) {
+var Products = (function () {
+    function Products(productName, productValue) {
         this.name = productName;
         this.value = productValue;
     }
-}
-const product = new Product('Playstation 5', 5000);
-console.log(product);
+    return Products;
+}());
+var products = [new Products('Playstation 5', 5000)];
+products.push(new Products('Playstation 2', 1000));
+products.push(new Products('Playstation 1', 300));
+console.log(products);
+products.every(function (product) { return product.value += 100; });
+products.forEach(function (product) { return console.log(product); });
 //# sourceMappingURL=first_code.js.map
--- scripts/first_code.ts
+++ scripts/first_code.ts
@@ -3,7 +3,7 @@ console.log("Hello world! This is my first TypeScript code!");
 const user = "John";
 console.log("Hello " + user + "!. Welcome!");
     
-class Product{
+class Products implements Product{
     name: string;
     value: number;
     constructor(productName: string, productValue: number) {
@@ -12,6 +12,15 @@ class Product{
     }
 }
 
-const product = new Product('Playstation 5', 5000)
-console.log(product);
+interface Product{
+    name: string;
+    value: number;
+}
 
+const products = [new Products('Playstation 5', 5000)]
+products.push(new Products('Playstation 2', 1000))
+products.push(new Products('Playstation 1', 300))
+console.log(products);
+//products.forEach(product => console.log(product));
+products.every(product => product.value+=100);
+products.forEach(product => console.log(product));
