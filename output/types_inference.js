a300dd4b073748e3decaa07d8663746bd4193780
Josias Valentim <JOSIAS.FIGUEIREDO-ext@ab-inbev.com>
2024-04-02 19:46:32 -0300

    Added auto compile task

--- output/types_inference.js
+++ output/types_inference.js
@@ -1,4 +1,6 @@
 "use strict";
 var userInfo = [1, 'John', new Date()];
 console.log(typeof userInfo);
+userInfo.push('Test', 22, new Date(22, 2, 2024));
+userInfo.map(function (info) { return console.log(info); });
 //# sourceMappingURL=types_inference.js.map
--- scripts/types_inference.ts
+++ scripts/types_inference.ts
@@ -4,5 +4,6 @@ console.log(typeof userInfo)
 
 userInfo.push('Test', 22, new Date(22, 2, 2024)) // Allowed
 // userInfo.push('Test 2', 23, true) // Not allowed
+userInfo.map(info => console.log(info))
 
 
