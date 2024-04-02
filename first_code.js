6064769bd2e0157fd1d9a84d02dd4a68ffd75170
Josias Valentim <JOSIAS.FIGUEIREDO-ext@ab-inbev.com>
2024-04-02 16:56:38 -0300

    TS Config

--- first_code.js
+++ first_code.js
@@ -1,3 +1,10 @@
 console.log("Hello world! This is my first TypeScript code!");
 var user = "John";
 console.log("Hello " + user + "!. Welcome!");
+var Product = /** @class */ (function () {
+    function Product(productName, productValue) {
+        this.name = productName;
+        this.value = productValue;
+    }
+    return Product;
+}());
--- first_code.ts
+++ first_code.ts
@@ -3,3 +3,11 @@ console.log("Hello world! This is my first TypeScript code!");
 const user = "John";
     console.log("Hello "+user+"!. Welcome!");
     
+class Product{
+    name: string;
+    value: number;
+    constructor(productName: string, productValue: number) {
+        this.name = productName;
+        this.value = productValue;
+    }
+}
