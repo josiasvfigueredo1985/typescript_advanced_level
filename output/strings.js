fb06ab62920adc3f6baf2614cb57b1cb28825ba6
Josias Valentim <JOSIAS.FIGUEIREDO-ext@ab-inbev.com>
2024-04-02 22:17:51 -0300

    .gitignore is now working

--- output/strings.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var company;
-company = "Microsoft Corporation";
-console.log(company);
-company = "Microsoft";
-console.log("Company is: ".concat(company));
-//# sourceMappingURL=strings.js.map
