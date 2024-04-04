578a9733b0ddc27e0ea81fd7f9d3e32b51941d81
Josias Valentim <JOSIAS.FIGUEIREDO-ext@ab-inbev.com>
2024-04-04 19:27:04 -0300

    Rules updated

--- src/scripts/first_code.ts
+++ src/scripts/first_code.ts
@@ -4,23 +4,27 @@ const user = 'John'
 console.log('Hello ' + user + '!. Welcome!')
 
 class Products implements Product {
-    name: string
-    value: number
-    constructor(productName: string, productValue: number) {
-        this.name = productName
-        this.value = productValue
-    }
+  name: string
+  value: number
+  constructor (productName: string, productValue: number) {
+    this.name = productName
+    this.value = productValue
+  }
 }
 
 interface Product {
-    name: string
-    value: number
+  name: string
+  value: number
 }
-
-const products = [new Products('Playstation 5', 5000)]
-products.push(new Products('Playstation 2', 1000))
-products.push(new Products('Playstation 1', 300))
+const prices = [5000, 1000, 300]
+const products = [new Products('Playstation 5', prices[0])]
+products.push(new Products('Playstation 2', prices[1]))
+products.push(new Products('Playstation 1', prices[2]))
 console.log(products)
-products.forEach((product) => console.log(product))
+products.forEach((product) => {
+  console.log(product)
+})
 products.every((product) => (product.value += 110))
-products.forEach((product) => console.log(product))
+products.forEach((product) => {
+  console.log(product)
+})
--- src/scripts/strings.ts
+++ src/scripts/strings.ts
@@ -1,14 +1,14 @@
 // const company = "Google Inc"
 // company = "Amazon" // Does not work
 interface company {
-    name: string
+  name: string
 }
 
 class Company implements Company {}
 
-let test_Camel: string
+let COMP: string
 COMP = 'Microsoft Corporation'
-console.log(COMP) //;
+console.log(COMP) // ;
 
 COMP = 'Googl' // Does work
 console.log(`Company is: ${COMP}`)
