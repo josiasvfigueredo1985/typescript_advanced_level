36f0f796afb34812500806c65373139cba676a46
Josias Valentim <JOSIAS.FIGUEIREDO-ext@ab-inbev.com>
2024-05-06 15:40:37 -0300

    Union type

--- src/scripts/union_types.ts
+++ src/scripts/union_types.ts
@@ -4,4 +4,16 @@ const paymentMethod = (
   return payType
 }
 
+let payment: 'paypal' | 'credit card'
+payment = 'paypal'
+console.log(paymentMethod(payment))
+payment = 'credit card'
+console.log(paymentMethod(payment))
 console.log(paymentMethod('pix'))
+
+let pixKey: number | string
+// eslint-disable-next-line @typescript-eslint/no-magic-numbers
+pixKey = 16659959
+console.log(pixKey)
+pixKey = 'myEmail@gmail.com'
+console.log(pixKey)
