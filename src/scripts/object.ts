8abcd9216504c4626715bbc9d183832c9d3f05fc
Josias Valentim <JOSIAS.FIGUEIREDO-ext@ab-inbev.com>
2024-05-06 16:42:24 -0300

    Object

---
+++ src/scripts/object.ts
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+let serverConfig: object = {
+  server: 'localhost',
+  ip: '127.0.0.1',
+  password: '484848kjhjkh',
+}
+console.log(serverConfig) // { server: 'localhost', ip: '127.0.0.1', password: '484848kjhjkh' }
+serverConfig = { sever: 'Google Cloud Platform' }
+console.log(serverConfig) // { sever: 'Google Cloud Platform' }
