0b2bff16072173aa4cd4fb351a59d919545d031f
Josias Valentim <JOSIAS.FIGUEIREDO-ext@ab-inbev.com>
2024-05-06 16:30:37 -0300

    Tuple

---
+++ src/scripts/tuple.ts
@@ -0,0 +1,19 @@
+/* eslint-disable @typescript-eslint/no-magic-numbers */
+/* eslint-disable prefer-const */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+let address: [string, string, number]
+
+address = ['Fascination St.', 'London', 333]
+// address = ['Fascination St.', 1, 333] // Type Error
+console.log(address)
+
+let weekDay: [string, string, string, string, string, string, string]
+weekDay = [
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+  'Sunday',
+]
