3f04d640d336bbb7a0a33de3ce032f5c2e48fe64
Josias Valentim <JOSIAS.FIGUEIREDO-ext@ab-inbev.com>
2024-08-15 21:07:46 -0300

    Any type

---
+++ src/scripts/any.ts
@@ -0,0 +1,15 @@
+// Avoid using this type on Typescript
+
+let value // Automatically inferred as any, same as "let value: any"
+console.log(typeof value) // undefined
+value = 'John Smith'
+console.log(typeof value) // string
+value = 1
+console.log(typeof value) // number
+value = true
+console.log(typeof value) // boolean
+// eslint-disable-next-line @typescript-eslint/no-magic-numbers
+value = [1, 2, 3, 4, 5]
+console.log(typeof value) // object
+value = { name: 'John', ahe: 35 }
+console.log(typeof value) // object
