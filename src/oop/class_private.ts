ed34857f2c0ea27435ff817ab0eac55eaf784898
Josias Valentim <JOSIAS.FIGUEIREDO-ext@ab-inbev.com>
2024-08-19 12:15:00 -0300

    Class - Private

---
+++ src/oop/class_private.ts
@@ -0,0 +1,34 @@
+/* eslint-disable @typescript-eslint/no-magic-numbers */
+class Bank {
+  private vaultMoneyQty = 10000
+
+  private withDrawFromVault(moneyQty: number): number | string {
+    if (this.vaultMoneyQty >= moneyQty) {
+      this.vaultMoneyQty -= moneyQty
+      return this.vaultMoneyQty
+    } else {
+      return 'Required amount not available!'
+    }
+  }
+
+  protected withDrawFromCashier(moneyQty: number): number | string {
+    return this.withDrawFromVault(moneyQty)
+  }
+
+  public withDrawFromATM(moneyQty: number): number | string {
+    return this.withDrawFromCashier(moneyQty)
+  }
+}
+
+const bankOfAmerica = new Bank()
+console.log(bankOfAmerica.withDrawFromATM(10001))
+
+class Bank24Hs extends Bank {
+  // Only protected and public methods are visible to the child class
+  withDrawl(moneyQty: number): number | string {
+    return this.withDrawFromCashier(moneyQty)
+  }
+}
+
+const myBank = new Bank24Hs()
+console.log(myBank.withDrawl(1000))
