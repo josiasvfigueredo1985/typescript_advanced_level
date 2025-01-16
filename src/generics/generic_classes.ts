e19353e5ad1c87bab4476e351676467526a145ed
JOSIAS VALENTIM DE FIGUEREDO <josiasvfigueredo@gmail.com>
2025-01-16 14:53:56 -0300

    Generic classes

---
+++ src/generics/generic_classes.ts
@@ -0,0 +1,63 @@
+interface IBaseRegistration {
+  name: string
+  readonly id: string
+  readonly createdAt: Date
+  readonly updatedAt: Date
+}
+
+interface IRegisterUser extends IBaseRegistration {
+  email: string
+  password: string
+}
+
+interface IRegisterCategory extends IBaseRegistration {
+  type: string
+}
+
+class BaseRegister<BaseInterface> {
+  create(data: BaseInterface): void {
+    console.log('Creating data', data)
+  }
+
+  read(id: number): BaseInterface {
+    console.log('Reading data with id:', id)
+    // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+    const data: BaseInterface = {} as BaseInterface
+    return data
+  }
+
+  update(id: number, updatedData: BaseInterface): BaseInterface {
+    console.log('Updating data with id:', id)
+    return updatedData
+  }
+
+  delete(id: number): boolean {
+    console.log('Deleting data with id:', id)
+    return true
+  }
+}
+class ModelUser extends BaseRegister<IRegisterUser> {}
+
+const newBaseUser = new ModelUser()
+newBaseUser.create({
+  name: 'John Doe',
+  email: 'john.doe@example.com',
+  password: 'password123',
+  id: '123abc',
+  createdAt: new Date(),
+  updatedAt: new Date(),
+})
+
+newBaseUser.read(1)
+
+class Movie extends BaseRegister<IRegisterCategory> {}
+
+const fiction = new Movie()
+fiction.create({
+  name: 'Fiction',
+  type: 'Category',
+  id: '456def',
+  createdAt: new Date(),
+  updatedAt: new Date(),
+})
+fiction.read(1)
